ff63dd92171ab18372a37acca08c031005b0a407
PatrycjaHomosapiens <45950819+PatrycjaHomosapiens@users.noreply.github.com>
2019-01-15 11:40:26 +0100

    Update and rename 07.py to Zmienne_konkatenacja.py

--- 07.py
+++
@@ -1,7 +0,0 @@
-imie = "Patrycja"
-nazwisko = "Matalewska"
-miejsceUrodzenia = "Katowice"
-dataUrodzenia = "15/03/1984"
-pesel = 12345
-
-print(imie + " " + nazwisko + " urodzona " + miejsceUrodzenia + " w dniu " + dataUrodzenia + " o peselu " + str(pesel) + ".")
---
+++ Zmienne_konkatenacja.py
@@ -0,0 +1,11 @@
+"""
+Napisz kod, w którym będziesz przechowywać następujące dane
+"""
+
+imie = "Patrycja"
+nazwisko = "Matalewska"
+miejsceUrodzenia = "Katowice"
+dataUrodzenia = "15/03/1984"
+pesel = 12345
+
+print(imie + " " + nazwisko + " urodzona " + miejsceUrodzenia + " w dniu " + dataUrodzenia + " o peselu " + str(pesel) + ".")
