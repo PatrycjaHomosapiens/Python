cd44e73a6a4bc60f28eee4204da01ac632f60ae6
PatrycjaHomosapiens <45950819+PatrycjaHomosapiens@users.noreply.github.com>
2019-01-15 11:53:13 +0100

    Update and rename 08.py to Zmienne_zad_zabronione.py

--- Zmienne_zad_zabronione.py
+++ Zmienne_zad_zabronione.py
@@ -1,16 +1,18 @@
 """
+Sprawdź czy można wykorzystać, jako zmienną nazwę zabronioną (and, if, break)
+"""
+
 and = 8
 print(and)
 
 zm = False
 print(type(zm))
 
-
 zm = 3.14159
 print(int(zm))
 print(round(zm,0))
+print(round(zm,1))
 print(round(zm,2))
-"""
 
 zm1 = bool(0) # fałsz
 zm2 = bool("")
