3537ce5beffd9f4d5dcbab22db167c84d7383207
PatrycjaHomosapiens <45950819+PatrycjaHomosapiens@users.noreply.github.com>
2019-01-16 12:34:09 +0100

    Update and rename 34.py to Listy_działaniaNaListach.py

--- Listy_działaniaNaListach.py
+++ Listy_działaniaNaListach.py
@@ -9,7 +9,7 @@ lista2 = [1, 2, 3]
 suma = lista1[0] + lista1[1] + lista1[2] + lista2[0] + lista2[1] + lista2[2]   # odwoływanie do indeksów
 print(suma)   # wynik to 12
 
-lista3 = [2,5,6]
+lista3 = [2, 5, 6]
 iloczyn = lista3[0] * lista3[1] * lista3[2]
 print(iloczyn)   # wynik to 60
 
