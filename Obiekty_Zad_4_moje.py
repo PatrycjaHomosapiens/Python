a47597715736ec846dbcad758a8c8633b23ad02d
PatrycjaHomosapiens <45950819+PatrycjaHomosapiens@users.noreply.github.com>
2019-01-24 16:42:43 +0100

    Add files via upload

--- Obiekty_Zad_4_moje.py
+++ Obiekty_Zad_4_moje.py
@@ -8,9 +8,48 @@
 # jesli nie mozna zrealizowac żądanej kwoty to bankomat zraca pustą listę
 
 # przyklad
-cash_machine = CashMachine()
+
+class CashMachine():
+    def __init__(self):
+        self._szuflada = []
+
+    def put_money(self, banknotes):
+        self._szuflada.extend(banknotes)
+        self._szuflada.sort(reverse=True)
+
+    def withdraw_money(self, amount):
+        wyplata = []
+        for papier in self._szuflada:
+            if papier + sum(wyplata) <= amount:
+                wyplata.append(papier)
+
+        if sum(wyplata) == amount:
+            for papier in wyplata:
+                self._szuflada.remove(papier)
+            return wyplata
+
+        return []
+
+
+cash_machine = CashMachine()        # to jest nasz __init__
 cash_machine.put_money([200,100,100,50])
 wyplata  = cash_machine.withdraw_money(150)
 print(wyplata) # ==> [100,50]
 druga_wyplata = cash_machine.withdraw_money(150)
 print(druga_wyplata) # ==> []
+
+
+
+
+# będę trzymać banknoty w strukturze listy
+'''
+cs = CashMachine()
+cs.put_money([20,20,20,50,100,200])
+wyplata = cs.withdraw_money(160)
+
+cs = CashMachine()
+cs.put_money([50,100,50,50,200])
+wyplata = cs.withdraw_money(300)    # nie wyplaci, bo banknoty sa źle poukładane
+'''
+
+
